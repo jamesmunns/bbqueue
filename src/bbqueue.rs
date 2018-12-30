8329d7feda748814d6b92340927fcaa44e32c037
James Munns <james.munns@ferrous-systems.com>
2018-12-30 03:25:09 +0100

    Works apparently with two tracks

--- src/bbqueue.rs
+++ src/bbqueue.rs
@@ -1,27 +1,30 @@
-type Result<T> = ::std::result::Result<T, ()>;
+pub type Result<T> = ::std::result::Result<T, ()>;
+use core::slice::from_raw_parts;
 
-struct BBQueue {
-    buf: [u8; 32], // TODO, ownership et. al
+#[derive(Debug)]
+pub struct BBQueue {
+    buf: [u8; 6], // TODO, ownership et. al
     active: Track,
     passive: Track,
 }
 
-struct BBBufIn {
-    buf: &'static [u8]
-}
-
-struct BBBufInHandle {
-
-}
-
-struct BBBufOut {
-
-}
-
-struct Track {
+#[derive(Debug)]
+pub struct Track {
     start: usize,
     reserve: usize,
-    committed: usize
+    committed: usize,
+}
+
+#[derive(Debug)]
+pub struct GrantW {
+    // TODO, how to tie this to the lifetime of BBQueue?
+    pub buf: &'static mut [u8],
+}
+
+#[derive(Debug)]
+pub struct GrantR {
+    // TODO, how to tie this to the lifetime of BBQueue?
+    pub buf: &'static [u8],
 }
 
 impl Track {
@@ -37,54 +40,107 @@ impl Track {
 impl BBQueue {
     pub fn new() -> Self {
         BBQueue {
-            buf: [0u8; 32],
+            buf: [0u8; 6],
             active: Track::new(),
             passive: Track::new(),
         }
     }
 
-    pub fn grant(&mut self, size: usize) -> Result<BBBufIn> {
-        if self.active.reserve + size < self.buf.len() {
+    pub fn grant<'a>(&mut self, sz: usize) -> Result<GrantW> {
+        if self.active.reserve != self.active.committed {
+            return Err(()); // GRANT IN PROCESS
+        }
+
+        if self.active.reserve + sz <= self.buf.len() {
             // alloc in primary
-        } else if size < self.active.start {
+        } else if sz < self.active.start {
             // Swap, now we're using primary
-            ::core::mem::swap(
-                &mut self.active,
-                &mut self.passive
-            );
+            ::core::mem::swap(&mut self.active, &mut self.passive);
         } else {
             return Err(());
         }
 
-        self.active.start += size;
+        let x = &mut self.buf.split_at_mut(self.active.reserve).1[..sz];
 
-        let x = BBBufIn {
-            buf: unsafe {
-                ::core::slice::from_raw_parts(
-                    &self.buf[self.active.start],
-                    size
-                )
-            },
-        };
+        self.active.reserve += sz;
 
-        Ok(x)
+        Ok(GrantW {
+            buf: unsafe { ::core::mem::transmute::<_, &'static mut _>(x) },
+        })
     }
 
+    pub fn commit(&mut self, used: usize, grant: GrantW) {
+        let len = grant.buf.len();
+        assert!(len >= used);
+        drop(grant);
+        self.active.reserve -= len - used;
 
+        // this probably goes last for thread safety?
+        self.active.committed += used;
+    }
+
+    pub fn read(&mut self) -> GrantR {
+        let avail_pas = self.passive.committed - self.passive.start;
+        let avail_act = self.active.committed - self.active.start;
+
+        match (avail_pas, avail_act) {
+            (p, _a) if p != 0 => {
+                // slice from passive first
+                GrantR {
+                    buf: unsafe { from_raw_parts(&self.buf[self.passive.start], avail_pas) },
+                }
+            }
+            (_p, a) if a != 0 => {
+                // slice from active second
+                GrantR {
+                    buf: unsafe { from_raw_parts(&self.buf[self.active.start], avail_act) },
+                }
+            }
+            _ => {
+                // YOU GET NOTHING
+                GrantR { buf: &[] }
+            }
+        }
+    }
+
+    pub fn release(&mut self, used: usize, grant: GrantR) {
+        assert!(used <= grant.buf.len());
+        drop(grant);
+        let avail_pas = self.passive.committed - self.passive.start;
+        let avail_act = self.active.committed - self.active.start;
+
+        match (avail_pas, avail_act) {
+            (p, _a) if p != 0 => {
+                self.passive.start += used;
+                if self.passive.start == self.passive.reserve {
+                    self.passive = Track::new();
+                }
+            }
+            (_p, a) if a != 0 => {
+                self.active.start += used;
+                if self.active.start == self.active.reserve {
+                    self.active = Track::new();
+                }
+            }
+            _ => {
+                panic!("nothing to free?");
+            }
+        }
+    }
 }
 
-impl BBBufIn {
-    pub fn commit(self, used: usize) -> BBBufInHandle {
-        unimplemented!()
-    }
+// impl BBBufIn {
+//     pub fn commit(self, used: usize) -> BBBufInHandle {
+//         unimplemented!()
+//     }
 
-    pub fn abort(self) {
-        // TODO, this is just drop? Needs to update upstream BBQueue
-    }
-}
+//     pub fn abort(self) {
+//         // TODO, this is just drop? Needs to update upstream BBQueue
+//     }
+// }
 
-impl BBBufOut {
-    pub fn release(self) {
-        // TODO, this is just drop? Needs to update upstream BBQuque
-    }
-}
+// impl BBBufOut {
+//     pub fn release(self) {
+//         // TODO, this is just drop? Needs to update upstream BBQuque
+//     }
+// }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,74 @@
 mod bbqueue;
+use crate::bbqueue::BBQueue;
 
 fn main() {
-    println!("Hello, world!");
+    let mut bb = BBQueue::new();
+
+    println!("init: {:?}", bb);
+    println!("read: {:?}", bb.read());
+
+    let x = bb.grant(4).unwrap();
+
+    println!("granted: {:?}", bb);
+    println!("read: {:?}", bb.read());
+
+    x.buf.copy_from_slice(&[1, 2, 3, 4]);
+
+    println!("filled: {:?}", bb);
+    println!("read: {:?}", bb.read());
+
+    bb.commit(4, x);
+
+    println!("committed: {:?}", bb);
+    let a = bb.read();
+    println!("read: {:?}", a);
+
+    bb.release(2, a);
+
+    println!("released: {:?}", bb);
+    println!("read: {:?}", bb.read());
+
+    let x = bb.grant(2).unwrap();
+
+    println!("granted: {:?}", bb);
+    println!("read: {:?}", bb.read());
+
+    x.buf.copy_from_slice(&[11, 12]);
+
+    println!("filled: {:?}", bb);
+    println!("read: {:?}", bb.read());
+
+    bb.commit(2, x);
+
+    println!("committed: {:?}", bb);
+    let a = bb.read();
+    println!("read: {:?}", a);
+
+    bb.release(2, a);
+
+    println!("released: {:?}", bb);
+    println!("read: {:?}", bb.read());
+
+    let x = bb.grant(3).unwrap();
+
+    println!("granted: {:?}", bb);
+    println!("read: {:?}", bb.read());
+
+    x.buf.copy_from_slice(&[21, 22, 23]);
+
+    println!("filled: {:?}", bb);
+    println!("read: {:?}", bb.read());
+
+    bb.commit(3, x);
+
+    println!("committed: {:?}", bb);
+    let a = bb.read();
+    println!("read: {:?}", a);
+
+    bb.release(2, a);
+
+    println!("released: {:?}", bb);
+    println!("read: {:?}", bb.read());
+
+    assert!(bb.grant(10).is_err());
 }
