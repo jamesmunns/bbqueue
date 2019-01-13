ce6f73398c1160fc9301ca080a9b768e11bee2cd
James Munns <james.munns@ferrous-systems.com>
2019-01-13 02:36:19 +0100

    Clean up tests a bit, less redundancy

--- ci/test.sh
+++ ci/test.sh
@@ -9,20 +9,23 @@ cargo build --target thumbv7em-none-eabihf --manifest-path core/Cargo.toml
 # Check doctests
 cargo test --manifest-path core/Cargo.toml
 
-# Test using a full std crate, short test with multiple threads (it's slow)
-cargo test --features="travisci" --manifest-path bbqtest/Cargo.toml -- --nocapture
-cargo test --features="travisci" --release --manifest-path bbqtest/Cargo.toml -- --nocapture
-
-export SHORT_TEST=""
-
 if [ $TRAVIS_RUST_VERSION = nightly ]; then
+    # Run ONLY the short test, also with a sanitizer
+
     export RUSTFLAGS="-Z sanitizer=thread"
     export RUST_TEST_THREADS=1
     export TSAN_OPTIONS="suppressions=$(pwd)/tsan-blacklist.txt"
     export SHORT_TEST="--features=travisci"
-fi
 
-# Test using a full std crate, long test with a single thread (it's faster)
-cargo test $SHORT_TEST --manifest-path bbqtest/Cargo.toml -- --nocapture --test-threads=1
-cargo test $SHORT_TEST --release --manifest-path bbqtest/Cargo.toml -- --nocapture --test-threads=1
+    cargo test --features="travisci" --manifest-path bbqtest/Cargo.toml -- --nocapture
+    cargo test --features="travisci" --release --manifest-path bbqtest/Cargo.toml -- --nocapture
+else
+    # Test using a full std crate, short test with multiple threads (it's slow)
+    cargo test --features="travisci" --manifest-path bbqtest/Cargo.toml -- --nocapture
+    cargo test --features="travisci" --release --manifest-path bbqtest/Cargo.toml -- --nocapture
+
+    # Test using a full std crate, long test with a single thread (it's faster)
+    cargo test --manifest-path bbqtest/Cargo.toml -- --nocapture --test-threads=1
+    cargo test --release --manifest-path bbqtest/Cargo.toml -- --nocapture --test-threads=1
+fi
 
