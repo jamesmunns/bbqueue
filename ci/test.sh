892381d5a28ff3a6e3a98e50944b66b9f638aae5
James Munns <james.munns@ferrous-systems.com>
2019-12-04 14:28:04 +0100

    Sanitizer is stable

--- ci/test.sh
+++ ci/test.sh
@@ -10,22 +10,15 @@ cargo build --target thumbv7em-none-eabihf --manifest-path core/Cargo.toml
 cargo test --manifest-path core/Cargo.toml --no-default-features --features atomic
 cargo test --manifest-path core/Cargo.toml --no-default-features --features thumbv6
 
-if [ $TRAVIS_RUST_VERSION = nightly ]; then
-    # Run ONLY the short test, also with a sanitizer
+export RUSTFLAGS="-Z sanitizer=thread"
+export RUST_TEST_THREADS=1
+export TSAN_OPTIONS="suppressions=$(pwd)/tsan-blacklist.txt"
 
-    export RUSTFLAGS="-Z sanitizer=thread"
-    export RUST_TEST_THREADS=1
-    export TSAN_OPTIONS="suppressions=$(pwd)/tsan-blacklist.txt"
+# Test using a full std crate, short test with multiple threads (it's slow)
+cargo test --features="travisci" --manifest-path bbqtest/Cargo.toml --target x86_64-unknown-linux-gnu -- --nocapture
+cargo test --features="travisci" --release --manifest-path bbqtest/Cargo.toml --target x86_64-unknown-linux-gnu -- --nocapture
 
-    cargo test --features="travisci,nightly" --manifest-path bbqtest/Cargo.toml -- --nocapture
-    cargo test --features="travisci,nightly" --release --manifest-path bbqtest/Cargo.toml -- --nocapture
-else
-    # Test using a full std crate, short test with multiple threads (it's slow)
-    cargo test --features="travisci" --manifest-path bbqtest/Cargo.toml -- --nocapture
-    cargo test --features="travisci" --release --manifest-path bbqtest/Cargo.toml -- --nocapture
-
-    # Test using a full std crate, long test with a single thread (it's faster)
-    cargo test --manifest-path bbqtest/Cargo.toml -- --nocapture --test-threads=1
-    cargo test --release --manifest-path bbqtest/Cargo.toml -- --nocapture --test-threads=1
-fi
+# Test using a full std crate, long test with a single thread (it's faster)
+cargo test --manifest-path bbqtest/Cargo.toml --target x86_64-unknown-linux-gnu -- --nocapture --test-threads=1
+cargo test --release --manifest-path bbqtest/Cargo.toml --target x86_64-unknown-linux-gnu -- --nocapture --test-threads=1
 
